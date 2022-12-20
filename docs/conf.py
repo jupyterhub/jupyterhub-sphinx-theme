ab775b57d340de8386d79490d4dfed800fc065b7
Chris Holdgraf <choldgraf@berkeley.edu>
2022-12-20 20:11:33 +0100

    Config

--- docs/conf.py
+++ docs/conf.py
@@ -15,8 +15,6 @@ extensions = [
     "myst_parser",
 ]
 
-source_suffix = ['.rst', '.md']
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
